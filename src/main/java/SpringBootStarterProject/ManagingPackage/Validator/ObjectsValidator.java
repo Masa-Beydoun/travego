22bd9d2a6b8dc0a12b84935551ff3e62e5b2661d
Wassem Tenbakji <wasee.tenbakji@gmail.com>
2024-07-07 06:59:14 +0300

    unImportant commit (for .idea file)

--- src/main/java/SpringBootStarterProject/ManagingPackage/Validator/ObjectsValidator.java
+++ src/main/java/SpringBootStarterProject/ManagingPackage/Validator/ObjectsValidator.java
@@ -5,6 +5,7 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 @Component
 public class ObjectsValidator<T> {
     private  final ValidatorFactory factory= Validation.buildDefaultValidatorFactory();
+    @Autowired
     private final Validator validator=factory.getValidator();
 
     public void validate(T ObjectTOValidate)
