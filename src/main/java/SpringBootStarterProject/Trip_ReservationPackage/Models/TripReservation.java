711b5d8724611ebda8cc8e01281c18823d6f6de7
kareemalmosafi <THEKAREEMBO@gmail.com>
2024-08-14 18:07:50 +0300

    Revert "fixing id Generated Value"
    This reverts commit 0f9a1ce96525948d27a891262ea37afd3cb4ace3.

--- src/main/java/SpringBootStarterProject/Trip_ReservationPackage/Models/TripReservation.java
+++ src/main/java/SpringBootStarterProject/Trip_ReservationPackage/Models/TripReservation.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class TripReservation {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
